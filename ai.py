1b5d3fd2619901c475511e06f4eb2177154b8ad5
Stanislav Ponomarev <sponomar@bbn.com>
2019-04-09 14:39:55 -0400

    Added cli interface

--- ai.py
+++ ai.py
@@ -1,10 +1,29 @@
 class AI:
     def __init__(self):
-        self.chat_log = open('chat_log.txt', 'a')
+        self.permissions = {'Hexorg': '*'}
+        self.commands = {'test': self.cmd_test, 
+                        'flush': self.cmd_flush}
 
     def command(self, nick, command):
-        print("CMD: {}: {}".format(nick, command))
+        if len(command) > 0:
+            command = command.split(' ')
+            args = command[1:]
+            command = command[0]
+        else:
+            return None
+        if nick not in self.permissions:
+            return "I shouldn't talk to strangers"
+        else:
+            if self.permissions[nick] != '*' and command not in self.permissions[nick]:
+                return "{} can't perform '{}'".format(nick, command)
+        if command not in self.commands:
+            return "Unknown command {}".format(command)
+
+        return self.commands[command](nick, args)
+
+    def cmd_test(self, nick, args):
+        return "test!"
 
     def learn(self, nick, text):
-        print("learn: {}: {}".format(nick, text))
-        self.chat_log.write('<{}>: {}\n'.format(nick, ''.join(text)))
+        with open('chat_log.txt', 'a') as f:
+            f.write('<{}>: {}\n'.format(nick, ''.join(text)))
---
+++ cli.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import ai
+
+class CLI:
+    def __init__(self):
+        self.is_running = True
+        self.ai = ai.AI()
+
+    def loop(self):
+        while self.is_running:
+            msg = input(">>> ")
+            print(self.ai.command('Hexorg', msg))
+
+            
+
+if __name__ == '__main__':
+    cli = CLI()
+    cli.loop()
